8d120b47c2efcf12afef1bfbe5dd974103564cda
tegarsaktirmdhn <tegarsakti011@gmail.com>
2024-04-01 00:17:48 +0700

    first commit

---
+++ product.php
@@ -0,0 +1,22 @@
+<?php
+$produk1 = array(
+          'foto' => 'img/samsunga55.png',
+          'nama' => 'Samsung Galaxy A55 5G',
+          'deskripsi' => 'SoC Exynos 1480 yang didukung GPU berteknologi AMD yang mumpuni .',
+          'harga' => 'Rp. 5.999.000'
+        );
+
+        $produk2 = array(
+          'foto' => 'img/samsunga35.png',
+          'nama' => 'Samsung Galaxy A35 5G',
+          'deskripsi' => 'Cocok untuk gaming dan keseharian, performa Exynos 1380 cukup tangguh.',
+          'harga' => 'Rp. 4.999.000'
+        );
+
+        $produk3 = array(
+          'foto' => 'img/samsungxcover7.png',
+          'nama' => 'Samsung Galaxy XCover 7',
+          'deskripsi' => 'Punya sertifikasi ketahanan militer, tahan terhadap benturan.',
+          'harga' => 'Rp. 4.999.000'
+        );
+?>
